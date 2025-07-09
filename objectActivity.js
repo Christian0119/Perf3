b6272899fbac5956b58985709fc44f0abd1328e7
Christian0119 <ishtian2006@gmail.com>
2025-07-09 10:08:54 +0800

    Create objectActivity.js

---
+++ objectActivity.js
@@ -0,0 +1,17 @@
+// Create car object
+let car = {
+  type: "Sedan",
+  model: "Camry",
+  color: "Red"
+};
+
+// Check object type
+console.log("Type of 'car':", typeof car);
+
+// Update type
+car.type = "Toyota";
+console.log("Updated car type:", car);
+
+// Add new property
+car.wheels = 4;
+console.log("Car with wheels:", car);
