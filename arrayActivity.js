ca9605dd0e05b46d7749ef3782098af73c4b74e5
Christian0119 <ishtian2006@gmail.com>
2025-07-09 10:07:48 +0800

    Create arrayActivity.js

---
+++ arrayActivity.js
@@ -0,0 +1,17 @@
+// Create array of favorite singers
+let favSingers = ["Taylor Swift", "Bruno Mars", "Adele"];
+
+// Log the first singer
+console.log("First favorite singer:", favSingers[0]);
+
+// Create array of favorite numbers
+let favNumbers = [7, 11, 23, 42];
+
+// Create mixed array
+let mixedArr = ["string", ["otherarray"], 123, true];
+
+// Access and log each item
+console.log("Item 1:", mixedArr[0]);
+console.log("Item 2:", mixedArr[1]);
+console.log("Item 3:", mixedArr[2]);
+console.log("Item 4:", mixedArr[3]);
